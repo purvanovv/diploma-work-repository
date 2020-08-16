ae768d54d630fe1c8e064052699e2017fda1af10
Viktor Parvanov <victor.parvanov@nevexis.com>
2020-08-16 20:45:00 +0300

    Added new params when creating announcement

--- carsell-services/src/main/java/tusofia/carsellservices/repository/AnnouncementRepositoryImpl.java
+++ carsell-services/src/main/java/tusofia/carsellservices/repository/AnnouncementRepositoryImpl.java
@@ -1,5 +1,6 @@
 package tusofia.carsellservices.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +12,9 @@ import org.springframework.stereotype.Repository;
 
 import tusofia.carsellservices.model.Announcement;
 import tusofia.carsellservices.model.MainCategory;
+import tusofia.carsellservices.util.DateUtils;
 import tusofia.carsellservices.util.SqlContainer;
+import tusofia.carsellservices.util.SqlUtils;
 
 @Repository
 public class AnnouncementRepositoryImpl implements AnnouncementRepository {
@@ -52,23 +55,26 @@ public class AnnouncementRepositoryImpl implements AnnouncementRepository {
 			params.addValue("coolingType", announcement.getCoolingType());
 			params.addValue("engineCategory", announcement.getEngineCategory());
 			params.addValue("price", announcement.getPrice());
-			params.addValue("currency", announcement.getCurrency());
-			params.addValue("dateOfManufacture", announcement.getDateOfManufacture());
+			params.addValue("currency", announcement.getCurrency().getValue());
+			params.addValue("dateOfManufacture", DateUtils.getSubmissionDate(announcement.getDateOfManufacture()));
 			params.addValue("mileage", announcement.getMileage());
 			params.addValue("color", announcement.getColor());
 			params.addValue("region", announcement.getRegion());
 			params.addValue("city", announcement.getCity());
-			params.addValue("periodId", announcement.getPeriodId());
-			params.addValue("adValidFrom", announcement.getAdValidFrom());
-			params.addValue("adValidTo", announcement.getAdValidTo());
+			params.addValue("validDays", announcement.getValidDays());
+			params.addValue("adValidFrom", DateUtils.getSubmissionDate(announcement.getAdValidFrom()));
+			Date validTo = DateUtils.addDays(announcement.getAdValidFrom(), announcement.getValidDays());
+			params.addValue("adValidTo", DateUtils.getSubmissionDate(validTo));
 			params.addValue("description", announcement.getDescription());
-			params.addValue("createdBy", announcement.getCreatedBy());
+			params.addValue("createdBy", announcement.getMetaProps().getCreatedBy());
+			params.addValue("createdOn", DateUtils.getSubmissionDate(announcement.getMetaProps().getCreatedOn()));
+			params.addValue("deleteFlag", SqlUtils.getSubmissionBoolean(announcement.getMetaProps().getDeleteFlag()));
 
 			KeyHolder keyHolder = new GeneratedKeyHolder();
 			String sql = SqlContainer.CREATE_ANNOUNCEMENT;
 			namedParameterJdbcTemplate.update(sql, params, keyHolder, new String[] { "ID" });
 
-			return (long) keyHolder.getKey();
+			return keyHolder.getKey().longValue();
 
 		} catch (Exception e) {
 			// TODO
