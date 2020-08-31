41665ed07b6c26183b60d8953700fa88ee252fa8
Viktor Parvanov <victor.parvanov@nevexis.com>
2020-08-31 08:07:19 +0300

    Added enums

---
+++ carsell-client/src/app/announcement/enums.ts
@@ -0,0 +1,61 @@
+export enum AirConditionType {
+    NO,
+    ELECTRICAL,
+    TO_THE_ENGINE
+}
+export enum ConditionType {
+    NEW,
+    USED,
+    ON_PARTS
+}
+export enum CoolingType {
+    WATER,
+    AIR
+}
+export enum Currency {
+    LEV,
+    USD,
+    EUR
+}
+export enum EmissionStandartType {
+    EURO_1,
+    EURO_2,
+    EURO_3,
+    EURO_4,
+    EURO_5,
+    EURO_6
+}
+export enum EngineCategoryType {
+    TACT_2,
+    TACT_4
+}
+export enum EngineType {
+    DIESEL,
+    GASOLINE,
+    HYBRID,
+    ELECTRIC
+}
+export enum GearboxType {
+    MANUAL,
+    AUTOMATIC,
+    SEMI_AUTOMATIC
+}
+export enum HeatingType {
+    NO,
+    ELECTRICAL,
+    GAS
+}
+export enum MaterialType {
+    ALUMINUM,
+    IRON,
+    WOOD,
+    CEMENT,
+    KEVLAR,
+    PVC,
+    PLASTIC
+}
+export enum ToiletType {
+    NO,
+    CHEMICAL,
+    ORDINARY
+}
