469bd878c71fd8c548ae4c384966f76770da0334
Viktor Parvanov <victor.parvanov@nevexis.com>
2020-09-06 13:01:29 +0300

    Update install script - new tables

--- carsell_db/install.sql
+++ carsell_db/install.sql
@@ -15,18 +15,22 @@ prompt create tables staging area
 -- -------------------------------
 
 @ddl/crtb/crtb_ANNOUNCEMENT_VEHICLE.sql
-@ddl/crtb/crtb_ENUM_ITEM_BASES.sql
 @ddl/crtb/crtb_MAIN_CATEGORIES.sql
 @ddl/crtb/crtb_SUB_CATEGORIES.sql
+@ddl/crtb/crtb_MAKE_MODEL.sql
+@ddl/crtb/crtb_REGIONS.sql
+@ddl/crtb/crtb_REGION_CITIES.sql
 
 -- -----------------------------------------------
 prompt create primary keys for tables staging area
 -- -----------------------------------------------
 
 @ddl/crpk/crpk_ANNOUNCEMENT_VEHICLE_PK.sql
-@ddl/crpk/crpk_ENUM_ITEM_BASES_PK.sql
 @ddl/crpk/crpk_MAIN_CATEGORIES_PK.sql
 @ddl/crpk/crpk_SUB_CATEGORIES_PK.sql
+@ddl/crpk/crpk_MAKE_MODEL_PK.sql
+@ddl/crpk/crpk_REGIONS_PK.sql
+@ddl/crpk/crpk_REGION_CITIES_PK.sql
 
 -- -----------------------------------------------
 prompt create foreign keys for tables staging area
@@ -34,6 +38,8 @@ prompt create foreign keys for tables staging area
 
 @ddl/crfk/crfk_ANNOUNCEMENT_VEHICLE_FK.sql
 @ddl/crfk/crfk_SUB_CATEGORIES_FK.sql
+@ddl/crfk/crfk_MAKE_MODEL_FK.sql
+@ddl/crfk/crfk_REGION_CITIES_FK.sql
 
 -- -----------------------------------------------
 prompt create sequences
@@ -58,6 +64,8 @@ prompt load data
 -- -----------------------------------------------
 @@dml/ins_MAIN_CATEGORIES.sql
 @@dml/ins_SUB_CATEGORIES.sql
-@@dml/ins_ENUM_ITEM_BASES.sql
+@@dml/ins_MAKE_MODEL.sql
+@@dml/ins_REGIONS.sql
+@@dml/ins_REGION_CITIES.sql
 
 DISCONNECT
