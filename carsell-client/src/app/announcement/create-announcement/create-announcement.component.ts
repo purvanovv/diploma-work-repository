6d60d972642942d50bd87bdf60b5d8767cc0ac45
Viktor Parvanov <victor.parvanov@nevexis.com>
2020-09-20 21:19:23 +0300

    Remove unused imports

--- carsell-client/src/app/announcement/create-announcement/create-announcement.component.ts
+++ carsell-client/src/app/announcement/create-announcement/create-announcement.component.ts
@@ -1,22 +1,4 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { MainCategory, SubCategory, Make } from '../models';
-import { AnnouncementService } from '../announcement.service';
-import { Observable, Subject, pipe, concat } from 'rxjs';
-import { tap, startWith, mergeMap } from 'rxjs/operators';
-import {
-  EngineType,
-  AirConditionType,
-  ConditionType,
-  CoolingType,
-  Currency,
-  EmissionStandartType,
-  EngineCategoryType,
-  GearboxType,
-  HeatingType,
-  MaterialType,
-  ToiletType,
-} from '../enums';
 import { MatStepper } from '@angular/material/stepper';
 
 @Component({
