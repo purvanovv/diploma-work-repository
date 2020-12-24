11582aa5bb5e8770c87b4e25c585337106ca81b0
Viktor Parvanov <victor.parvanov@nevexis.com>
2020-12-24 20:10:05 +0200

    Implement simple pagination

--- carsell-client/src/app/announcement/list-search-result/list-search-result.component.ts
+++ carsell-client/src/app/announcement/list-search-result/list-search-result.component.ts
@@ -9,13 +9,50 @@ import { AnnouncementPreview } from '../models';
   styleUrls: ['./list-search-result.component.scss']
 })
 export class ListSearchResultComponent implements OnInit {
-  announcements: AnnouncementPreview[];
+  selected: AnnouncementPreview[];
+  itemsPerPage = 8;
+  isLoading: boolean;
+  currentPageIndex = 1;
+
+  pagesData: Map<number, AnnouncementPreview[]>;
   constructor(private announcementStore: AnnouncementStoreService) { }
 
   ngOnInit(): void {
+    this.isLoading = true;
     this.announcementStore.getAnnouncements$().pipe(tap((announcements: AnnouncementPreview[]) => {
-      this.announcements = announcements;
+      this.initPagesData(announcements);
+      this.selected = this.pagesData.get(this.currentPageIndex);
+      this.isLoading = false;
     })).subscribe();
+  }
+
+  initPagesData(announcements: AnnouncementPreview[]) {
+    const countOfPages = Math.ceil(announcements.length / this.itemsPerPage);
+    this.pagesData = new Map();
+    for (let pageIndex = 1; pageIndex <= countOfPages; pageIndex++) {
+      const itemsPerPage = Math.min(this.itemsPerPage, announcements.length);
+      this.pagesData.set(pageIndex, announcements.splice(0, itemsPerPage));
+    }
+    console.log(this.pagesData);
+  }
+
+  select(pageIndex: number) {
+    this.currentPageIndex = pageIndex;
+    this.selected = this.pagesData.get(this.currentPageIndex);
+  }
+
+  prev() {
+    if (this.currentPageIndex > 1) {
+      this.currentPageIndex--;
+      this.selected = this.pagesData.get(this.currentPageIndex);
+    }
+  }
+
+  next() {
+    if (this.currentPageIndex < this.pagesData.size) {
+      this.currentPageIndex++;
+      this.selected = this.pagesData.get(this.currentPageIndex);
+    }
   }
 
 }
