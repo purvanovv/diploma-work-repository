bf656fb115cffc2fcceb7932cd962c96357a013b
Viktor Parvanov <victor.parvanov@nevexis.com>
2020-12-25 11:31:56 +0200

    Finished pagination

--- carsell-client/src/app/announcement/list-search-result/list-search-result.component.ts
+++ carsell-client/src/app/announcement/list-search-result/list-search-result.component.ts
@@ -9,11 +9,13 @@ import { AnnouncementPreview } from '../models';
   styleUrls: ['./list-search-result.component.scss']
 })
 export class ListSearchResultComponent implements OnInit {
+  pages: number[] | undefined = undefined;
+
   selected: AnnouncementPreview[];
   itemsPerPage = 8;
   isLoading: boolean;
   currentPageIndex = 1;
-
+  maxPageIndexes = 4;
   pagesData: Map<number, AnnouncementPreview[]>;
   constructor(private announcementStore: AnnouncementStoreService) { }
 
@@ -21,9 +23,41 @@ export class ListSearchResultComponent implements OnInit {
     this.isLoading = true;
     this.announcementStore.getAnnouncements$().pipe(tap((announcements: AnnouncementPreview[]) => {
       this.initPagesData(announcements);
+      this.initPageIndexes();
       this.selected = this.pagesData.get(this.currentPageIndex);
       this.isLoading = false;
     })).subscribe();
+  }
+
+  initPageIndexes() {
+    if (this.pages === undefined) {
+      const totalPageIndexes = Math.min(this.pagesData.size, this.maxPageIndexes);
+      const firstPageIndex = this.currentPageIndex;
+      const lastPageIndex = firstPageIndex + totalPageIndexes - 1;
+      this.pages = [];
+      for (let index = firstPageIndex; index <= lastPageIndex; index++) {
+        this.pages.push(index);
+      }
+    } else {
+      const totalPageIndexes = Math.min(this.pagesData.size, this.maxPageIndexes);
+      if (this.currentPageIndex >= this.pages[this.pages.length - 1] && this.currentPageIndex <= this.pagesData.size) {
+        const lastPageIndex = Math.min(this.currentPageIndex + totalPageIndexes - 1, this.pagesData.size);
+        const firstPageIndex = lastPageIndex - totalPageIndexes + 1 < 1 ? 1 : lastPageIndex - totalPageIndexes + 1;
+        this.pages = [];
+        for (let index = firstPageIndex; index <= lastPageIndex; index++) {
+          this.pages.push(index);
+        }
+      }
+      else if (this.currentPageIndex <= this.pages[0] && this.currentPageIndex >= 1) {
+        const firstPageIndex = this.currentPageIndex - totalPageIndexes + 1 < 1 ? 1 : this.currentPageIndex - totalPageIndexes + 1;
+        const lastPageIndex = Math.min(firstPageIndex + totalPageIndexes - 1, this.pagesData.size);
+        this.pages = [];
+        for (let index = firstPageIndex; index <= lastPageIndex; index++) {
+          this.pages.push(index);
+        }
+      }
+    }
+
   }
 
   initPagesData(announcements: AnnouncementPreview[]) {
@@ -33,17 +67,19 @@ export class ListSearchResultComponent implements OnInit {
       const itemsPerPage = Math.min(this.itemsPerPage, announcements.length);
       this.pagesData.set(pageIndex, announcements.splice(0, itemsPerPage));
     }
-    console.log(this.pagesData);
   }
 
   select(pageIndex: number) {
+    console.log(pageIndex);
     this.currentPageIndex = pageIndex;
+    this.initPageIndexes();
     this.selected = this.pagesData.get(this.currentPageIndex);
   }
 
   prev() {
     if (this.currentPageIndex > 1) {
       this.currentPageIndex--;
+      this.initPageIndexes();
       this.selected = this.pagesData.get(this.currentPageIndex);
     }
   }
@@ -51,8 +87,17 @@ export class ListSearchResultComponent implements OnInit {
   next() {
     if (this.currentPageIndex < this.pagesData.size) {
       this.currentPageIndex++;
+      this.initPageIndexes();
       this.selected = this.pagesData.get(this.currentPageIndex);
     }
+  }
+
+  showFirst(): boolean {
+    return this.pages[0] > 1;
+  }
+
+  showLast(): boolean {
+    return this.pages[this.pages.length - 1] < this.pagesData.size;
   }
 
 }
